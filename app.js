489e2d704c5b34857207728d633da7389212ea81
JonCTack <jon.c.tackett@gmail.com>
2022-12-15 12:01:03 -0600

    third commit html wont load in loop

--- app.js
+++ app.js
@@ -29,27 +29,31 @@ enemyFleet.newEnemy()
 enemyFleet.newEnemy()
 enemyFleet.newEnemy()
 enemyFleet.newEnemy()
-
+//USS ship and 6 enemies made
 let ussShipHull = document.querySelector("#USS_Ship #USS_Hull")
 ussShipHull.innerText = ussShip.hull
 let enemyFleetContain = document.querySelectorAll(".enemy .enemy_hull")
 for (let i = 0;i < enemyFleetContain.length; i++){
     enemyFleetContain[i].innerText = enemyFleet.fleet[i].hull
 }
-
+//This displays the USS hull and each enemy's hull
 const startBattle = () => {
     let gameWon = true
     let retreat = false
     let enemy = enemyFleet.fleet
+    //this is for shortening the following
     for (let i = 0; i < enemy.length; i++){
         if(ussShip.hull < 1){
             break
         }
         while(enemy[i].hull > 0){
             let response = window.prompt("would you like to continue? type no to stop", "yes")
+            //defaulting yes is to help the player understand the default action, but they can type anything as long as it isn't no
             if (response == null){
+                //this is to catch pressing the cancel button instead of submitting
             }
-            else if (response.toLowerCase() == ("no" || "stop")){
+        
+            else if (response.toLowerCase() == ("no")){
                console.log("retreating")
                 retreat = true
                 break
@@ -58,12 +62,18 @@ const startBattle = () => {
             ussShip.shootShip(enemy[i])
             for (let i = 0;i < enemyFleetContain.length; i++){
                 enemyFleetContain[i].innerText = enemyFleet.fleet[i].hull
+                if (+enemyFleetContain[i].innerText < 0){
+                    enemyFleetContain[i].innerText = 0
+                    //if the hull is less than 0, it displays as 0
+                }
             }
+                //this updates the health of the enemy ships
             if (enemy[i].hull < 1){
                 break
             }
             enemy[i].shootShip(ussShip)
             ussShipHull.innerText = ussShip.hull
+                //this updates the health of the USS Ship
             if (ussShip.hull < 1){
                 gameWon = false;
                 break
