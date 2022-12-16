9fd343c2d81aecbdb43d974866b897702df61d93
JonCTack <jon.c.tackett@gmail.com>
2022-12-16 10:16:26 -0600

    sixth commit, enemies in flex, points and missle system

--- app.js
+++ app.js
@@ -13,7 +13,20 @@ class Ship {
             console.log("a miss.")
             return "no"
         }
+        //the returns allow us to call the method in a string, allowing us to dynamically access the firepower of enemy ships for the status screens
         
+    }
+    missileShip(target){
+        if (gamePoints>5){
+            if(Math.random() < (this.accuracy)*1.3){
+                target.hull -= (this.firepower)*2;
+                console.log(`a missile strike for ${this.firepower*2}`)
+                return this.firepower
+            } else {
+                console.log("a miss.")
+                return "no"
+            }
+        }
     }
 };
 class EnemyFleet{
@@ -24,7 +37,7 @@ class EnemyFleet{
         let enemyShip = new Ship(Math.floor((Math.random()*4)+3),
         Math.floor((Math.random()*3+2)),
         Math.floor(((Math.random()*3)+6))/10);
-        this.fleet.push(enemyShip)
+        this.fleet.push(enemyShip);
     }
 }
 let ussShip = new Ship(20, 5, .7);
@@ -38,6 +51,8 @@ enemyFleet.newEnemy()
 //USS ship and 6 enemies made
 let ussShipHull = document.querySelector("#USS_Ship #USS_Hull")
 ussShipHull.innerText = ussShip.hull
+let ussPoints = document.querySelector("#gamePoints")
+
 let enemyFleetHulls = document.querySelectorAll(".enemy .enemy_hull")
 let enemyFleetImgSrcArray = document.querySelectorAll(".enemy img")
 for (let i = 0;i < enemyFleetHulls.length; i++){
@@ -99,6 +114,7 @@ for (let i = 0;i < enemyFleetHulls.length; i++){
 //     }
 
 // }
+let gamePoints = 0;
 const startBattle = () => {
     if (gameWon == true||gameWon == false){
         //this checks if a game  has been played and resets all values
@@ -126,7 +142,11 @@ const startBattle = () => {
     gameButton.className = "hidden";
     shootButton.className = "";
     runButton.className = "";
+    if(gamePoints > 5 ){
+     missileButton.className = "";
+    }
     console.clear()
+    console.log(gamePoints)
 }
 let i = 0;
 const shootBattle = () => {
@@ -136,12 +156,15 @@ const shootBattle = () => {
     let enemy = enemyFleet.fleet;
     if(ussShip.hull < 1){
         gameWon = false
+        i = 0
+        //this fixes bugs in edges cases such as ensuring if we lost we start at ship one next game and we can't continue the game if we lost our health
     }
     console.log("shooting enemy ship")
     youShooterScreen.className=""
     youShooterScreen.innerText="shooting enemy ship"
     if (i >= enemy.length){
         i = 0
+        //in the case of a game restart after a win, this ensures that you aren't targeting the first ship
     }
     youStatScreen.className=""
     youStatScreen.innerText = ussShip.shootShip(enemy[i]) + " damage dealt";
@@ -155,34 +178,121 @@ const shootBattle = () => {
     //this updates the health of the enemy ships
     if (enemy[i].hull < 1){
         enemyFleetImgSrcArray[i].src = "./images/enemy_ship_dead.png"
-        i++
+        i++;
+        gamePoints++;
+        ussPoints.innerText = gamePoints;
         runButton.className = "";
         if (i >= enemy.length){
-            gameWon = true
+            gameWon = true;
         }
         } else {
             console.log("enemy is shooting")
             theyShooterScreen.className = "";
             theyStatScreen.className = "";
-            theyShooterScreen.innerText="enemy is shooting"
+            theyShooterScreen.innerText="enemy is shooting";
             theyStatScreen.innerText = enemy[i].shootShip(ussShip) + " damage taken";
-            ussShipHull.innerText = ussShip.hull
+            ussShipHull.innerText = ussShip.hull;
                 //this updates the health of the USS Ship
             if (ussShip.hull < 1){
                 gameWon = false;
+                i = 0
             }
         }
+     if(gamePoints > 5 ){
+            missileButton.className = "";
+    }
     if (gameWon == true){
         winScreen.className="";
         gameButton.className = "";
         shootButton.className = "hidden";
         runButton.className = "hidden";
+        missileButton.className = "hidden";
+        gamePoints++;
+        ussPoints.innerText = gamePoints;
     } else if (gameWon == false){
         loseScreen.className="";
         gameButton.className = "";
         shootButton.className = "hidden";
         runButton.className = "hidden";
+        missileButton.className = "hidden";
+        gamePoints = 0;
     }
+
+}
+//this is the same function but it shoots missiles instead
+const missileBattle = () => {
+    runButton.className = "hidden";
+    theyShooterScreen.className = "hidden";
+    theyStatScreen.className = "hidden";
+    let enemy = enemyFleet.fleet;
+    if(ussShip.hull < 1){
+        gameWon = false
+        i = 0
+        //this fixes bugs in edges cases such as ensuring if we lost we start at ship one next game and we can't continue the game if we lost our health
+    }
+    console.log("shooting missile at enemy ship")
+    youShooterScreen.className=""
+    youShooterScreen.innerText="shooting enemy ship"
+    if (i >= enemy.length){
+        i = 0
+        //in the case of a game restart after a win, this ensures that you aren't targeting the first ship
+    }
+    youStatScreen.className=""
+    youStatScreen.innerText = ussShip.missileShip(enemy[i]) + " damage dealt";
+    gamePoints -= 5
+    for (let i = 0;i < enemyFleetHulls.length; i++){
+        enemyFleetHulls[i].innerText = enemyFleet.fleet[i].hull
+     if (+enemyFleetHulls[i].innerText < 0){
+        enemyFleetHulls[i].innerText = 0
+        //if the hull is less than 0, it displays as 0
+        }
+    }
+    //this updates the health of the enemy ships
+    if (enemy[i].hull < 1){
+        enemyFleetImgSrcArray[i].src = "./images/enemy_ship_dead.png"
+        i++;
+        gamePoints++;
+        ussPoints.innerText = gamePoints;
+        runButton.className = "";
+        if (i >= enemy.length){
+            gameWon = true;
+        }
+        } else {
+            console.log("enemy is shooting")
+            theyShooterScreen.className = "";
+            theyStatScreen.className = "";
+            theyShooterScreen.innerText="enemy is shooting";
+            theyStatScreen.innerText = enemy[i].shootShip(ussShip) + " damage taken";
+            ussShipHull.innerText = ussShip.hull;
+                //this updates the health of the USS Ship
+            if (ussShip.hull < 1){
+                gameWon = false;
+                i = 0
+            }
+        }
+     if(gamePoints > 5 ){
+            missileButton.className = "";
+    }
+    if(gamePoints < 5 ){
+        missileButton.className = "hidden";
+    }
+    if (gameWon == true){
+        winScreen.className="";
+        gameButton.className = "";
+        shootButton.className = "hidden";
+        runButton.className = "hidden";
+        missileButton.className = "hidden";
+        gamePoints++;
+        ussPoints.innerText = gamePoints;
+    } else if (gameWon == false){
+        loseScreen.className="";
+        gameButton.className = "";
+        shootButton.className = "hidden";
+        runButton.className = "hidden";
+        missileButton.className = "hidden";
+        gamePoints = 0;
+    }
+
 }
 
 const runFromBattle = () => {
@@ -196,6 +306,7 @@ const runFromBattle = () => {
 let gameWon = undefined;
 let gameButton = document.getElementById("start")
 let shootButton = document.getElementById("shoot")
+let missileButton = document.getElementById("missile")
 let runButton = document.getElementById("retreat")
 let winScreen = document.getElementById("victory")
 let loseScreen = document.getElementById("defeat")
@@ -205,5 +316,6 @@ let theyStatScreen = document.getElementById("they-status")
 let theyShooterScreen = document.getElementById("they-shoot")
 gameButton.addEventListener("click", function(){startBattle()});
 shootButton.addEventListener("click", function(){shootBattle()});
+missileButton.addEventListener("click", function(){missileBattle()});
 runButton.addEventListener("click", function(){runFromBattle()});
 //12_16 to do: flexbox enemies, have point system, buy off points, maybe boss, maybe make pretty
